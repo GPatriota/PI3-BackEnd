6777da276bb90b6b5b121235e9e63cd4220761f8
GPatriota <gpspatriota@gmail.com>
2025-11-24 00:51:26 -0300

    feat: deploy configs

---
+++ src/main/java/com/habitus/apipi/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.habitus.apipi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOriginPattern("*");
+
+        config.addAllowedMethod("*");
+
+        config.addAllowedHeader("*");
+
+        config.setAllowCredentials(true);
+
+        config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
